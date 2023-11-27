452d8e7015e56e55585bca67f76845ed7f90f8d3
SAID <SAID@HP>
2023-11-27 12:46:41 +0300

    one2

--- pythonGit/urls.py
+++ pythonGit/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('pythonGitApp.urls')),
     path('two/', include('pythonGitApp2.urls')),
 ]
