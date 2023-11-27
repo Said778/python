5776e6c57845996a8e158df7c5d9e0fcc6ea64d0
SAID <SAID@HP>
2023-11-27 12:41:59 +0300

    app2 change

--- pythonGit/urls.py
+++ pythonGit/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('pythonGitApp.urls')),
+    path('two/', include('pythonGitApp2.urls')),
 ]
